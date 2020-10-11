d044ebe4069aae54a45d5bf57b1c6629c3e2b9ad
jdehning <jonas.dehning@ds.mpg.de>
2020-10-12 01:48:29 +0200

    Change name of potential

--- pymc4/flow/transformed_executor.py
+++ pymc4/flow/transformed_executor.py
@@ -61,7 +61,7 @@ def make_untransformed_model(dist, transform, state):
             transform.inverse_log_det_jacobian, sampled_transformed_value
         )
         coef = 1.0
-    yield distributions.Potential(potential_fn, coef=coef, name = transformed_scoped_name + "_pot")
+    yield distributions.Potential(potential_fn, coef=coef, name = transformed_scoped_name + "_Potential")
     # 3. return value to the user
     return sampled_untransformed_value
 
@@ -98,7 +98,7 @@ def make_transformed_model(dist, transform, state):
             state.transformed_values[transformed_scoped_name],
         )
         coef = 1.0
-    yield distributions.Potential(potential_fn, coef=coef, name=transformed_scoped_name+"_pot")
+    yield distributions.Potential(potential_fn, coef=coef, name=transformed_scoped_name+"_Potential")
     # 3. final return+yield will return untransformed_value
     # as it is stored in state.values
     # Note: we need yield here to make another checks on name duplicates, etc
